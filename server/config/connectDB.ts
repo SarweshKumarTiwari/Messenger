d942577cf650d162e7290cc80d6b71611f17a90d
SarweshKumarTiwari <sarweshtiwari14@gmail.com>
2023-06-18 13:12:04 +0530

    set uri of mongodb in .env file

--- server/config/connectDB.ts
+++ server/config/connectDB.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
-
-const URI:string="mongodb://localhost:27017/Messenger";
+import dotenv from "dotenv";
+dotenv.config();
+const URI:string=process.env.DB_URI||"mongodb://localhost:27017/Messenger";
 
 const connectUsers=()=>{
     mongoose.set('strictQuery', true);
